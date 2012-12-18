2f3c68faaaf6d3fb268466b7916ad2e960d76046
Paul Winkler <paul@bit.ly>
2012-12-18 18:57:04 +0000

    Added embedly preview URL and domain name of link

--- assumptionator/assumptionator/views.py
+++ assumptionator/assumptionator/views.py
@@ -1,8 +1,12 @@
 from pyramid.view import view_config
 import bitly_api
-
+import logging
+import requests
+import urlparse
 
 api_root = 'https://api-ssl.bitly.com/v3/'
+
+THUMBNAIL_SIZE = 140
 
 
 @view_config(route_name='home', renderer='templates/index.pt')
@@ -15,10 +19,28 @@ def my_view(request):
 def link_info(request):
     urls = request.GET.getall('shortUrl')
     params = {'shortUrl': urls}
-    data = bitly_api.fetch(api_root + 'info', params=params)['data']
-    for info in data['info']:
+    data = bitly_api.fetch(api_root + 'info', params=params)['data']['info']
+    for info in data:
+        # TODO: fewer API calls?
         # For now, clicks for all time.
-        clicks = bitly_api.fetch(api_root + 'link/clicks', params={'link': info['short_url']})
+        short_url = info['short_url']
+        clicks = bitly_api.fetch(api_root + 'link/clicks', params={'link': short_url})
         info['clicks'] = clicks['data']['link_clicks']
-    return {'data': data}
+        try:
+            expanded = bitly_api.fetch(api_root + 'expand', params={'shortUrl': short_url})
+            parsed = urlparse.urlparse(expanded['data']['expand'][0]['long_url'])
+            info['domain'] = parsed.hostname or ''
+        except (requests.HTTPError, KeyError, IndexError):
+            logging.exception("Error fetching long url for %r" % short_url)
+            info['domain'] = ''
+        info['preview'] = ''
+        try:
+            preview = bitly_api.fetch(api_root + 'link/preview',
+                                      params={'link': short_url,
+                                              'thumbnail_width': THUMBNAIL_SIZE,
+                                              'thumbnail_height': THUMBNAIL_SIZE})
+            info['preview'] = preview['data']['images'][0]['thumbnail_url']
+        except (requests.HTTPError, KeyError):
+            logging.exception("Error fetching embedly preview for %r" % short_url)
 
+    return {'data': data}
