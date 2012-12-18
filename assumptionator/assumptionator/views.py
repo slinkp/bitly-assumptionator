4236dc5e5cba69a6cf3abceadcc0c3f132d9d170
Paul Winkler <paul@bit.ly>
2012-12-18 16:54:26 +0000

    renamed index template; call bitly api

--- assumptionator/assumptionator/views.py
+++ assumptionator/assumptionator/views.py
@@ -1,6 +1,10 @@
 from pyramid.view import view_config
+import bitly_api
+
+api_root = 'https://api-ssl.bitly.com/v3/'
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/index.pt')
 def my_view(request):
-    return {'project': 'assumptionator'}
+    data = bitly_api.fetch(api_root + 'user/info')
+    return {'project': 'assumptionator', 'bitly': data}
